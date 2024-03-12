6474abb32e1d045a8ac8bb76e9c8d5bad9e21d26
Kim Rune Møller <kimrune@hotmail.no>
2024-03-12 15:49:53 +0100

    badge design change

--- src/Badges.jsx
+++ src/Badges.jsx
@@ -10,12 +10,15 @@ function Badges() {
         rel="noreferrer"
         href="https://coursera.org/share/19b2fafdf15662fbdeb3935a89087bfd"
       >
-        <img
-          src={meta}
-          width={100}
-          alt="Meta Front-End Developer Certificate"
-        />
-        Meta Front-End Developer Certificate
+        <div className=" border-0 border-r border-r-orange-100 h-full w-24 flex items-center">
+          <img
+            src={meta}
+            width={100}
+            className="h-12 max-w-44"
+            alt="Meta Front-End Developer Certificate"
+          />
+        </div>
+        <div>Meta Front-End Developer Certificate</div>
       </a>
       <a
         className="sm:text-lg border border-orange-100 flex gap-4 my-2 sm:max-w-72 h-24 rounded-md p-2  hover:bg-slate-200 text-slate-900 items-center"
@@ -23,8 +26,15 @@ function Badges() {
         target="_blank"
         rel="noreferrer"
       >
-        <img src={cloud101} width={100} alt="AWS Cloud 101" />
-        AWS Cloud Computing 101
+        <div className=" border-0 border-r border-r-orange-100 h-full w-24 flex items-center">
+          <img
+            src={cloud101}
+            width={100}
+            className=" max-w-44"
+            alt="AWS Cloud 101"
+          />
+        </div>
+        <div>AWS Cloud Computing 101</div>
       </a>
     </div>
   );
