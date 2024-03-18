a2ee09f4ee22f0543d9cf54c17c7d6b65ab689fd
Kim Rune Møller <kimrune@hotmail.no>
2024-03-18 12:59:09 +0100

    design change home page link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,6 @@
 import profile from "../assets/Compress/profile2.jpg";
 import github from "../assets/github.png";
 import linkedin from "../assets/linkedin.png";
-import portfolio from "../assets/portfolio.png";
 import Button from "./Button";
 import Summary from "./Summary";
 import { useContext } from "react";
@@ -61,8 +60,7 @@ function Contact() {
         </div>
         <div className=" col-span-3 rounded p-1 py-2 flex gap-5 text-gray-950">
           <span className="hidden print:block">
-            linkedin.com/in/kim-rune-moller | github.com/kimrm |
-            portfolio.kimrune.dev
+            linkedin.com/in/kim-rune-moller | github.com/kimrm | kimrune.dev
           </span>
           <Button
             title="LinkedIn Profile"
@@ -75,12 +73,25 @@ function Contact() {
             <img src={github} width={25} alt="GitHub profile icon" />
             <span className="hidden sm:block">GitHub</span>
           </Button>
-          <Button
-            title="Frontend portfolio"
-            linkUrl="https://portfolio.kimrune.dev"
-          >
-            <img src={portfolio} width={25} alt="Portfolio icon" />
-            <span className="text-sm sm:text-base">Portfolio</span>
+          <Button title="Frontend portfolio" linkUrl="https://kimrune.dev">
+            <svg
+              fill="none"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
+              />
+            </svg>
+            <span className="text-sm sm:text-base">
+              {language === "no" ? "Hjemmeside" : "Home page"}
+            </span>
           </Button>
         </div>
       </div>
