efce71f4d87b3761e6e669518958e510d64595bb
Kim Rune Møller <kimrune@hotmail.no>
2024-10-31 18:36:04 +0100

    adds title to dark/light mode button

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,8 @@ const svg = {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="currentColor"
-      className="w-5 h-5 inline-block"
-    >
-      <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
-    </svg>
-  ),
-  dark: (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
       className="w-4 h-4 inline-block"
+      title="Light mode is on"
     >
       <path
         fillRule="evenodd"
@@ -32,6 +23,17 @@ const svg = {
       />
     </svg>
   ),
+  dark: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-5 h-5 inline-block"
+      title="Dark mode is on"
+    >
+      <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
+    </svg>
+  )
 };
 
 function App() {
@@ -47,7 +49,7 @@ function App() {
     const element = ref.current;
     const canvas = await html2canvas(element, {
       scale: 1,
-      useCORS: true,
+      useCORS: true
     });
 
     const pdfWidth = 210;
@@ -59,7 +61,7 @@ function App() {
     const pdf = new jsPDF({
       orientation: "portrait",
       unit: "mm",
-      format: [pdfWidth, pdfHeight],
+      format: [pdfWidth, pdfHeight]
     });
 
     const canvasRatio = canvas.height / canvas.width;
@@ -135,6 +137,15 @@ function App() {
             className="flex h-10 items-center justify-center"
             onClick={toggleColorScheme}
             aria-label="Toggle color scheme"
+            title={
+              systemColorScheme === "dark"
+                ? language === "en"
+                  ? "Dark mode is on"
+                  : "Mørk modus er på"
+                : language === "en"
+                ? "Light mode is on"
+                : "Lys modus er på"
+            }
           >
             {svg[systemColorScheme === "dark" ? "light" : "dark"]}
           </button>
